63f0a36a1e0b439701b43b7855db561c2b356816
Pannaga01 <53700154+Pannaga01@users.noreply.github.com>
2020-01-16 20:40:49 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,21 @@
+
+var mysql = require('mysql');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : 'nivvi',
 	database : 'User_Registration'
+});
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... nn");
+} else {
+    console.log("Error connecting database ... nn");
+}
 });
 
 var app = express();
@@ -16,29 +29,31 @@ app.get('/', function(request, response) {
 
 
 app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
-	if (username && password) {
-  		connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-  		if (error) {
-    			console.log("error ocurred",error);
-    			res.send({
-      			"code":400,
-      			"failed":"error ocurred"
-    			})
-  		}else{
-    			console.log('The solution is: ', results);
-    			res.send({
-     	 		"code":200,
-      			"success":"user registered sucessfully"
+	var users={
+		"email" : request.body.emailid,
+		"password" : request.body.password,
+	}
+	
+  	connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+  	if (error) {
+    		console.log("error ocurred",error);
+    		res.send({
+      		"code":400,
+      		"failed":"error ocurred"
+    		})
+  	}else{
+    		console.log('The solution is: ', results);
+    		res.send({
+     	 	"code":200,
+      		"success":"user registered sucessfully"
         	});
-  		}
+  	}
 	});
- }});
+ });
 
 
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
+app.get('/home/pannagasreya/project/select.html', function(request, response) {
+	if (console.log) {
 		response.send('Welcome back, ' + request.session.username + '!');
 	} else {
 		response.send('Please login to view this page!');
@@ -46,4 +61,4 @@ app.get('/home', function(request, response) {
 	response.end();
 });
 
-app.listen(6000);
+app.listen(6700);
